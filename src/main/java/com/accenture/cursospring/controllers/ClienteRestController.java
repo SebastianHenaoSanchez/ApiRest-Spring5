098ef9f4e5ec12c4b7b493535e79c06a48617c5b
Sebastian Henao <sebastian.henao5741@gmail.com>
2019-10-25 12:10:27 -0500

    Uso de cors para permitir de donde vengan las peticiones y a que tipo de métodos pueden acceder

--- src/main/java/com/accenture/cursospring/controllers/ClienteRestController.java
+++ src/main/java/com/accenture/cursospring/controllers/ClienteRestController.java
@@ -3,13 +3,17 @@ package com.accenture.cursospring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.cursospring.models.entity.Cliente;
 import com.accenture.cursospring.models.services.IClienteService;
 
+//se usa para permitir de donde me hacen las peticiones, en este caso ponemos la de angular
+@CrossOrigin(origins= {"http://localhost:4200"},methods= {RequestMethod.GET})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
