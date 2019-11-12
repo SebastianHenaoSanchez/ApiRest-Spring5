b5bd9a2d62be8d172e8d6b68363410076e41c318
Sebastian Henao <sebastian.henao5741@gmail.com>
2019-11-12 16:16:15 -0500

    Generacion de nombre aleatorio para la imagen y evitar nombres repetidos

--- src/main/java/com/accenture/cursospring/controllers/ClienteRestController.java
+++ src/main/java/com/accenture/cursospring/controllers/ClienteRestController.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -192,7 +193,7 @@ public class ClienteRestController {
 		Map<String, Object> response = new HashMap<>();
 		
 		if(!archivo.isEmpty()) {
-			String nombreArchivo = archivo.getOriginalFilename();
+			String nombreArchivo = UUID.randomUUID().toString() +"_" + archivo.getOriginalFilename().replace(" ", "");
 			Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
 			
 			try {
